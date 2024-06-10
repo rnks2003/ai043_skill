c90bcc9b1cb3a28455f144b85d043aa4d875ee50
Ravi <rnks2003@gmail.com>
2024-06-10 13:58:55 +0530

    rnks

--- test.c
+++ test.c
@@ -6,73 +6,22 @@ void display(){
     glColor3f(1.0,1.0,1.0);
     glPointSize(1.0);
     glBegin(GL_POINTS);
-    for(float x= 0; x<100;x+=2)
-        for(float y =0;y<600;y+=2)
-            glVertex2f(x,y);
-    for(float x=100;x<200;x+=2)
-        for(float y=0;y<100;y+=2){
-            glVertex2f(x,y+200);
-            glVertex2f(x,y+500);
-        }
-    for(float x=150;x<250;x+=2)
-        for(float y=300;y<500;y+=2)
-            glVertex2f(x,y);
-
-    for(float x =150;x<250;x+=2)
-        for(float y=200;y>250-x ;y-=2)
-            glVertex2f(x,y);
-
-    for(float x = 300;x<375;x+=2)
-        for(float y=0;y<600;y+=2)
-            glVertex2f(x,y);
-
-    for(float x=300;x<500;x+=2)
-        for(float y=598;y>700-x;y-=2){
-            float o = x-400;
-            if(y> 550-o){
-                continue;
+    glBegin(GL_POINTS);
+    for(float x=250.0;x<500;x+=2)
+        for(float y=0.0;y<250;y+=2){
+            if(x+y < 400 && x-y > 100){
+                glColor3f(1.0,0.0,0.0);
             }
-            glVertex2f(x,y);
-        }
-    for(float x=500;x<575;x+=2)
-        for(float y=0;y<600;y+=2)
-            glVertex2f(x,y);
-
-    for(float x=625;x<700;x+=2)
-        for(float y=0;y<600;y+=2)
-            glVertex2f(x,y);
-
-    for(float x=700;x<900;x+=2)
-        for(float y=598;y>1000-x;y-=2){
-            float o = x-700;
-            if(y> 550-o){
-                continue;
+            else if(x+y < 570 && x-y > 0){
+                glColor3f(0.0,1.0,0.0);
             }
-            glVertex2f(x,y-200);
-        }
-
-    for(float x=700;x<900;x+=2)
-        for(float y=300;y<600;y+=2){
-            if(y>x-300){
-                continue;
+            else{
+                glColor3f(0.0,0.0,1.0);
             }
-            glVertex2d(x,y);
-        }
+            glVertex2d(x+y,y);
+            glVertex2d(x-y,y);
 
-    for(float x=1000;x<1300;x+=2)
-        for(float y=0;y<100;y+=2){
-            glVertex2f(x,y);
-            glVertex2f(x,y+200);
-            glVertex2f(x,y+500);
         }
-    for(float x=1000;x<1100;x+=2)
-        for(float y=300;y<600;y+=2)
-            glVertex2f(x,y);
-    
-    for(float x=1200;x<1300;x==2)
-        for(float y=100;y<300;y+=2)
-            glVertex2f(x,y);
-    
     glEnd();
     glFlush();
 }
@@ -80,7 +29,7 @@ void init(){
     glClearColor(0.0,0.0,0.0,1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0,1400,-100,700);
+    gluOrtho2D(0,800,-100,300);
 }
 void main(int argc, char** argv){
     glutInit(&argc,argv);
